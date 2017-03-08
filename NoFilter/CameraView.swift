c84a6409d87dc67bd9eed72fcb33299952ad9b95
Lawrence Martin <lawrence.martin@cantire.com>
2017-03-08 18:52:11 -0500

    Added in firebase upload method

--- NoFilter/CameraView.swift
+++ NoFilter/CameraView.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class CameraView: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
     
     @IBOutlet weak var imageViewer: UIImageView!
     
+    var storage = FIRStorage.storage()
+    var storageRef:FIRStorageReference!
+    var data: Data!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+    
+        storage = FIRStorage.storage().reference(forURL:"gs://projectreall-35e59.appspot.com")
+        userStorage = storage.child("users")
         
         let image=UIImagePickerController()
         image.delegate=self
@@ -33,12 +41,24 @@ class CameraView: UIViewController,UINavigationControllerDelegate,UIImagePickerC
            }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
        imageViewer.image=info[UIImagePickerControllerOriginalImage] as! UIImage;
+        self.data = UIImageJPEGRepresentation(self.imageViewer.image!, 0.5)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func postImage(_ sender: UIButton) {
         
+        // Create a reference to the file you want to upload
+        let uploadImageRef = storageRef.child("images/userUploads.jpg")
         
+        // Upload the file to the path "images/rivers.jpg"
+        let uploadTask = uploadImageRef.put(self.data, metadata: nil) { (metadata, error) in
+            guard let metadata = metadata else {
+                // Uh-oh, an error occurred!
+                return
+            }
+            // Metadata contains file metadata such as size, content-type, and download URL.
+            let downloadURL = metadata.downloadURL
+        }
         
     }
 }
